8c9f41a000e0d0e55ff03d408cb1aa3817d617bd
Tep1g <gamerluis.m@gmail.com>
2024-09-24 01:44:02 -0500

    add simple config

---
+++ configs/simple.py
@@ -0,0 +1,39 @@
+from gem5.components.boards.simple_board import SimpleBoard
+from gem5.components.cachehierarchies.ruby.mesi_two_level_cache_hierarchy import (
+    MESITwoLevelCacheHierarchy,
+)
+from gem5.components.memory.single_channel import SingleChannelDDR4_2400
+from gem5.components.processors.cpu_types import CPUTypes
+from gem5.components.processors.simple_processor import SimpleProcessor
+from gem5.isas import ISA
+from gem5.resources.resource import BinaryResource
+from gem5.simulate.simulator import Simulator
+
+cache_hierarchy = MESITwoLevelCacheHierarchy(
+    l1d_size="16kB",
+    l1d_assoc=8,
+    l1i_size="16kB",
+    l1i_assoc=8,
+    l2_size="256kB",
+    l2_assoc=16,
+    num_l2_banks=1,
+)
+
+# Give ourselves 4GB of the best DDR4 the simulator has to offer
+memory = SingleChannelDDR4_2400(size="4GB")
+
+# X86 CPU
+processor = SimpleProcessor(cpu_type=CPUTypes.TIMING, isa=ISA.X86, num_cores=1)
+
+board = SimpleBoard(
+    clk_freq="3GHz",
+    processor=processor,
+    memory=memory,
+    cache_hierarchy=cache_hierarchy,
+)
+
+binary = BinaryResource(local_path="tests/test-progs/sum/bin/sum")
+board.set_se_binary_workload(binary)
+
+simulator = Simulator(board=board)
+simulator.run()
