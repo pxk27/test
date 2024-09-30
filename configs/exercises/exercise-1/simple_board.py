191dc6239387943f12c2f139bbc90d0319fb1376
Tep1g <gamerluis.m@gmail.com>
2024-09-30 18:31:39 -0500

    switch to classic cache

--- configs/exercises/exercise-1/simple_board.py
+++ configs/exercises/exercise-1/simple_board.py
@@ -1,7 +1,6 @@
 from gem5.components.boards.simple_board import SimpleBoard
-from gem5.components.cachehierarchies.ruby.mesi_two_level_cache_hierarchy import (
-    MESITwoLevelCacheHierarchy,
-)
+from gem5.components.cachehierarchies.classic.no_cache import NoCache
+from gem5.components.cachehierarchies.classic.private_l1_cache_hierarchy import PrivateL1CacheHierarchy
 from gem5.components.memory.single_channel import SingleChannelDDR4_2400
 from gem5.components.processors.cpu_types import CPUTypes
 from gem5.components.processors.simple_processor import SimpleProcessor
@@ -9,15 +8,14 @@ from gem5.isas import ISA
 from gem5.resources.resource import BinaryResource
 from gem5.simulate.simulator import Simulator
 
-cache_hierarchy = MESITwoLevelCacheHierarchy(
-    l1d_size="16kB",
-    l1d_assoc=8,
-    l1i_size="16kB",
-    l1i_assoc=8,
-    l2_size="256kB",
-    l2_assoc=16,
-    num_l2_banks=1,
-)
+user_input = input("Configure CPU with cache?: ")
+if user_input == "yes":
+    cache_hierarchy = PrivateL1CacheHierarchy(
+        l1d_size="32kB",
+        l1i_size="32kB",
+    )
+else:
+    cache_hierarchy = NoCache()
 
 # Give ourselves 4GB of the best DDR4 the simulator has to offer
 memory = SingleChannelDDR4_2400(size="4GB")
