622fc5031b592f87acfb6871808d3d695f3b6559
jisupyoon <jisup.yoon@samsung.com>
2024-11-21 17:26:20 +0900

    mem-cache: fix cache bug
    fix bug where only first instruction gets executed when multiple instructions were depending on single cache line load when using classic cache model

--- src/mem/cache/cache.cc
+++ src/mem/cache/cache.cc
@@ -807,7 +807,7 @@ Cache::serviceMSHRTargets(MSHR *mshr, const PacketPtr pkt, CacheBlk *blk)
                 // responseLatency is the latency of the return path
                 // from lower level caches/memory to an upper level cache or
                 // the core.
-                completion_time += clockEdge(responseLatency) +
+                completion_time += curTick() + responseLatency +
                     (transfer_offset ? pkt->payloadDelay : 0);
 
                 assert(!tgt_pkt->req->isUncacheable());
