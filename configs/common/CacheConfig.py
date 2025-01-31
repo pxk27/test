b992f1495b7742df8cae6f79a7847bfb4247fdf3
dm68 <dm68@illinois.edu>
2025-01-31 15:11:14 -0600

    Description of your changes

--- configs/common/CacheConfig.py
+++ configs/common/CacheConfig.py
@@ -127,17 +127,20 @@ def config_cache(options, system):
     if options.l2cache and options.elastic_trace_en:
         fatal("When elastic trace is enabled, do not configure L2 caches.")
 
-    if options.l2cache:
-        # Provide a clock for the L2 and the L1-to-L2 bus here as they
-        # are not connected using addTwoLevelCacheHierarchy. Use the
-        # same clock as the CPUs.
-        system.l2 = l2_cache_class(
-            clk_domain=system.cpu_clk_domain, **_get_cache_opts("l2", options)
-        )
 
-        system.tol2bus = L2XBar(clk_domain=system.cpu_clk_domain)
-        system.l2.cpu_side = system.tol2bus.mem_side_ports
-        system.l2.mem_side = system.membus.cpu_side_ports
+    if options.l3cache:
+
+        system.l3 = L3Cache(
+            clk_domain=system.cpu_clk_domain, **_get_cache_opts("l3", options)
+            )
+
+        system.tollcbus = L2XBar(clk_domain=system.cpu_clk_domain)
+        system.l3.cpu_side = system.tollcbus.mem_side_ports
+        system.l3.mem_side = system.membus.cpu_side_ports
+
+        #If there is no L3, connect L2 to membus directly
+        #else:
+            #system.l2.mem_side = system.membus.cpu_side_ports
 
     if options.memchecker:
         system.memchecker = MemChecker()
@@ -146,6 +149,7 @@ def config_cache(options, system):
         if options.caches:
             icache = icache_class(**_get_cache_opts("l1i", options))
             dcache = dcache_class(**_get_cache_opts("l1d", options))
+            l2cache = l2_cache_class(**_get_cache_opts("l2", options))
 
             # If we are using ISA.X86 or ISA.RISCV, we set walker caches.
             if ObjectList.cpu_list.get_isa(options.cpu_type) in [
@@ -175,9 +179,8 @@ def config_cache(options, system):
 
             # When connecting the caches, the clock is also inherited
             # from the CPU in question
-            system.cpu[i].addPrivateSplitL1Caches(
-                icache, dcache, iwalkcache, dwalkcache
-            )
+            system.cpu[i].addTwoLevelCacheHierarchy(icache, dcache, l2cache, iwalkcache, dwalkcache)
+
 
             if options.memchecker:
                 # The mem_side ports of the caches haven't been connected yet.
@@ -209,9 +212,9 @@ def config_cache(options, system):
                 )
 
         system.cpu[i].createInterruptController()
-        if options.l2cache:
+        if options.l3cache:
             system.cpu[i].connectAllPorts(
-                system.tol2bus.cpu_side_ports,
+                system.tollcbus.cpu_side_ports,
                 system.membus.cpu_side_ports,
                 system.membus.mem_side_ports,
             )
